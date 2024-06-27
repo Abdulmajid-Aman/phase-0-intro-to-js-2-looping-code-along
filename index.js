2415d03c7256b1ad81f4124bf99b4202f8a7348b
Abdulmajid Aman Hussein <abdulmajid.aman.student@moringaschool.com>
2024-06-27 17:08:24 +0300

    loops array

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+let array = ["Guadalupe" , "Ollie" , "Aki"]
+let what = 'surprise'
+
+function writeCards(array ,what) {
+    let newArray = []
+    for (let i = 0; i < array.length ; i ++) {
+        newArray.push(`Thank you, ${array[i]}, for the wonderful ${what} gift!`)
+    }
+    return newArray
+
+}
+
+writeCards(["Guadalupe" , "Ollie" , "Aki"] , 'surprise')
+
+
+function countDown() {
+    let count = 0
+    while (count < 11) {
+        console.log(count)
+        count ++
+    }
+
+}
